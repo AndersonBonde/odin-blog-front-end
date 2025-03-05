e1dcf8732901f7e9a58d721eed16239d4f4a00ab
Anderson <andersonbonde@gmail.com>
2025-03-05 10:52:50 -0300

    Refactor + reload commnets after create and delete

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 import './styles.css';
 import './scripts/headerInfo.js';
 import './scripts/postsList.js';
-import './scripts/commentSection.js';
--- src/scripts/postsList.js
+++ src/scripts/postsList.js
@@ -1,41 +1,51 @@
-const { buildComment } = require('./commentSection');
+async function loadSideBarLinks() {
+  const posts = await fetchAllBlogPosts();
+  createAndDisplayLinks(posts);
 
-// Load the list of blog posts
-const postListUl = document.querySelector('.post-list');
+  // Load most recent blogPost on first page load
+  displayPost(posts[0]);
+}
+loadSideBarLinks();
 
-fetch('http://localhost:3000/posts', {
-  method: 'GET',
-  headers: { 'Content-Type': 'application/json' }
-})
-.then((response) => response.json())
-.then((data) => {
-  // Sort all posts to show most recent on top
-  const sorted = data.blogPosts.sort((a, b) => {
-    return new Date(b.uploadAt).getTime() - new Date(a.uploadAt).getTime();
-  });
+async function fetchAllBlogPosts(sortedByDate = true) {
+  return fetch('http://localhost:3000/posts', {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' }
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    if (sortedByDate) {
+      return data.blogPosts.sort((a, b) => {
+        return new Date(b.uploadAt).getTime() - new Date(a.uploadAt).getTime();
+      })
+    }
 
-  // Add eventListener to each link
-  sorted.forEach((post) => {
+    return posts = data.blogPosts;
+  })
+  .catch((err) => console.log(err));
+}
+
+function createAndDisplayLinks(allPosts) {
+  const postListUl = document.querySelector('.post-list');
+  postListUl.innerHTML = '';
+
+  allPosts.forEach((post) => {
     const li = document.createElement('li');
     const link = document.createElement('a');
-
+  
     link.textContent = post.title;
     link.href = '/';
     li.appendChild(link);
     postListUl.appendChild(li);
-
+  
     link.addEventListener('click', (e) => {
       e.preventDefault();
-
+  
       localStorage.setItem('post', JSON.stringify(post));
       displayPost(post);
     });
-  });
-
-  // Load most recent post after loading page for the first time
-  displayPost(sorted[0]);
-})
-.catch((err) => console.log(err));
+  })
+}
 
 // Display post info after clicking it's link
 const postTitle = document.querySelector('.post-title');
@@ -43,7 +53,7 @@ const postAuthor = document.querySelector('.post-author');
 const postContent = document.querySelector('.post-content');
 const commentSection = document.querySelector('.comments');
 
-function displayPost(post) {
+async function displayPost(post) {
   const span = document.createElement('span');
   span.classList.add('posted-at');
 
@@ -55,19 +65,105 @@ function displayPost(post) {
 
   postContent.textContent = post.content;
 
-  // Load all comments from clicked post
-  fetch(`http://localhost:3000/posts/${post.id}/comments`, {
+  // Fetch and display comments from post
+  displayCommentsOnPage(post.id);
+}
+
+async function displayCommentsOnPage(postId) {
+  // Erase all
+  commentSection.innerHTML = '';
+  const comments = await fetchPostComments(postId);
+  
+  comments.forEach((comment) => {
+    buildComment(comment);
+  })
+}
+
+async function fetchPostComments(postId) {
+  // Fetch all comments from postId
+  return fetch(`http://localhost:3000/posts/${postId}/comments`, {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' }
   })
   .then((response) => response.json())
   .then((data) => {
-    commentSection.innerHTML = '';
-
-    data.comments.forEach((comment) => {
-      buildComment(comment);
-    })
-    // TODO? Load comments by most recent first
+    return data = data.comments;
   })
   .catch((err) => console.log(err));
 }
+
+// Build a comment dynamically
+const { DateTime } = require('luxon');
+
+function buildComment(comment) {
+  const commentDiv = document.createElement('div');
+  const authorH = document.createElement('h4');
+  const postedAtSpan = document.createElement('span');
+  const deleteButton = document.createElement('button');
+  const contentDiv = document.createElement('div');
+
+  commentDiv.classList.add('comment-card');
+  authorH.classList.add('comment-author');
+  postedAtSpan.classList.add('comment-posted-at');
+  deleteButton.classList.add('comment-delete-button');
+  contentDiv.classList.add('comment-content');
+
+  authorH.innerText = `${comment.author ? comment.author.firstname : 'anonymous'}`;
+  const date = new Date(comment.uploadAt);
+  postedAtSpan.innerText = DateTime.fromJSDate(date).toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+  deleteButton.innerText = 'X';
+  contentDiv.innerText = comment.content;
+
+  deleteButton.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const response = confirm('Are you sure you want to delete this comment?');
+
+    if (response) {
+      fetch(`http://localhost:3000/posts/delete/comment/${comment.id}`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.message)
+        const postId = JSON.parse(localStorage.getItem('post')).id;
+        displayCommentsOnPage(postId);
+      })
+      .catch((err) => console.log(err));
+    }
+  })
+
+  commentDiv.appendChild(authorH);
+  commentDiv.appendChild(contentDiv);
+  authorH.appendChild(postedAtSpan);
+  authorH.appendChild(deleteButton);
+
+  commentSection.appendChild(commentDiv);
+}
+
+// Post a new comment
+const commentForm = document.querySelector('#comment-form');
+
+commentForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  const comment = document.getElementById('write-a-comment').value;
+  const postId = JSON.parse(localStorage.getItem('post')).id;
+  const user = JSON.parse(localStorage.getItem('user'));
+  const authorId = user ? user.id : undefined;
+  
+  fetch('http://localhost:3000/posts/create/comment', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ comment, postId, authorId })
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data.message);
+    displayCommentsOnPage(postId);
+  })
+  .catch((err) => console.log(err));
+
+  commentForm.reset();
+});
